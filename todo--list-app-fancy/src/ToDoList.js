58c33b98fcf9592506f545b10c1cdc7fc4581a44
Calvin Bruch <calvinbcoding@gmail.com>
2019-09-04 21:57:41 -0600

    functional react todo app for wwebsite, needs styling

--- todo--list-app-fancy/src/ToDoList.js
+++ todo--list-app-fancy/src/ToDoList.js
@@ -1,20 +1,55 @@
 import React, { Component } from 'react';
-//import NewItemForm from './NewItemForm';
+import NewTodoForm from './NewTodoForm';
+import Todo from './Todo';
 	class ToDoList extends Component {
 		constructor(props) {
 			super(props);
-			this.state = {todos: [{ task: "Feed The Dogs"}, {task: "Water The Garden"}]}
-		};
+			this.state = {
+				todos: []
+			};
+			this.create = this.create.bind(this);
+			this.remove = this.remove.bind(this);
+			this.update = this.update.bind(this);
+		}
+		create(newTodo) {
+			this.setState({
+				todos: [...this.state.todos, newTodo]
+			});
+		}
+		remove(id){
+			this.setState({
+				todos: this.state.todos.filter(t => t.id !== id)
+			});
+		}
+		update(id, updatedTask){
+			const updatedTodos = this.state.todos.map(todo => {
+				if(todo.id === id){
+					return {...todo, task: updatedTask}
+				}
+				return todo;
+			});
+			this.setState({ todos: updatedTodos });
+
+		}
 		render(){
+			const todos = this.state.todos.map(todo => {
+				return (
+					<Todo 
+						key={todo.id} 
+						id={todo.id} 
+						task={todo.task} 
+						removeTodo={this.remove} 
+						updateTodo={this.update}
+					/>
+				);
+			});
 			return (
 				<div>
 					<h1>Todo List!</h1>
-					<ul>
-						<li>Todo 1</li>
-						<li>Todo 2</li>
-					</ul>
+					<NewTodoForm createTodo={this.create} />
+					<ul>{todos}</ul>
 				</div>
-			)
+			);
 		}
 	}
 export default ToDoList;
